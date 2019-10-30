f509ae818c5f9df4592f9b25d11ef9a5a9fe2007
david3464 <51375293+david3464@users.noreply.github.com>
2019-10-30 15:23:01 -0400

    1

--- routes/functions.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('functions/1PhotoAnimation/3CircleRotate', { layout: 'hero_layout' });
-});
-
-module.exports = router;
